05a7b723e66ff84abe995e1c1c65f669fb1f2080
Brandon Tran <brandontran@berkeley.edu>
2020-03-22 14:55:53 -0700

    revised controls

--- control.js
+++ control.js
@@ -1,5 +1,5 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
@@ -8,8 +8,8 @@ slider.oninput = function () {
   circle_number = parseInt(this.value);
 }
 
-var slider2 = document.getElementById("myRange2");
-var output2 = document.getElementById("demo2");
+let slider2 = document.getElementById("myRange2");
+let output2 = document.getElementById("demo2");
 output2.innerHTML = slider2.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
